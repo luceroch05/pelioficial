9643b579b9d0ee11dc1ae820d350da4d0e4854a3
john <johnzorrillx@gmail>
2023-11-14 16:01:56 -0500

    añadi cosas

--- AlquilerPelas/src/main/java/com/proyecto/alquilerpelas/AlquilerPelas.java
+++ AlquilerPelas/src/main/java/com/proyecto/alquilerpelas/AlquilerPelas.java
@@ -10,6 +10,7 @@ package com.proyecto.alquilerpelas;
  */
 import com.proyecto.alquilerpelas.paneles.LoginPanel;
 import com.proyecto.alquilerpelas.paneles.LoginPanel1;
+import com.proyecto.alquilerpelas.paneles.pane;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.*;
@@ -24,6 +25,37 @@ public class AlquilerPelas{
         }
     });*/
 
+    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(pane.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(pane.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(pane.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(pane.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new pane().setVisible(true);
+            }
+        });
+        
+    /*
     JFrame ventana;
     ventana = new JFrame("Alquiler de pela");
     ventana.add(new LoginPanel());
@@ -33,7 +65,7 @@ public class AlquilerPelas{
 
     ventana.setVisible(true);
     
-     
+     */
     }
 
 }
