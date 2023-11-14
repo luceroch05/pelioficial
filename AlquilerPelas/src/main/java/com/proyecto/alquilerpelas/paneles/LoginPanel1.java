9fe78ccaf3d97637af9273f1d5af30e0b350f8cb
leo <leopb77@gmail.com>
2023-11-13 19:23:11 -0500

    Test1

--- AlquilerPelas/src/main/java/com/proyecto/alquilerpelas/paneles/LoginPanel1.java
+++ AlquilerPelas/src/main/java/com/proyecto/alquilerpelas/paneles/LoginPanel1.java
@@ -11,7 +11,6 @@ package com.proyecto.alquilerpelas.paneles;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
