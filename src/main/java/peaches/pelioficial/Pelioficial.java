4e3c3545fffee3c9d571c323e86f0927c98cb320
lucero changra <luc3r0.ch4ngr4.99@gmail.com>
2023-11-15 11:11:34 -0500

    el oficial

---
+++ src/main/java/peaches/pelioficial/Pelioficial.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package peaches.pelioficial;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
+import com.formdev.flatlaf.intellijthemes.FlatCyanLightIJTheme;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Lucero
+ */
+public class Pelioficial {
+
+    public static void main(String[] args) {
+         FlatCyanLightIJTheme.setup();
+    // UIManager.put( "Button.arc", );
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new framePrincipal().setVisible(true);
+            }
+        });
+    }
+}
